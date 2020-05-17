09ed48193ba21425550f700d57ce51a65d9e0eb6
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-17 12:47:42 +0800

    feature
    添加Web服务器，并配置为默认启动客户端代码

---
+++ src/BlazorDevExpress/BlazorDevExpress.Shared/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorDevExpress
+{
+    public class Game
+    {
+        public string Home { get; set; }
+        public string Away { get; set; }
+        public DateTime Time { get; set; }
+        public string Judge { get; set; }
+        public string Weather { get; set; }
+    }
+}
