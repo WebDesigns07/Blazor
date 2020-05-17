db2c27004446c96eef110a5a30ba89ff0565b9fc
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-17 23:23:01 +0800

    feature
    DataGrid绑定

---
+++ src/BlazorDevExpress/BlazorDevExpress.Shared/Club.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDevExpress
+{
+    public class Club
+    {
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+        public string Coach { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- src/BlazorDevExpress/BlazorDevExpress.Shared/Game.cs
+++ src/BlazorDevExpress/BlazorDevExpress.Shared/Game.cs
@@ -4,8 +4,8 @@ namespace BlazorDevExpress
 {
     public class Game
     {
-        public string Home { get; set; }
-        public string Away { get; set; }
+        public Club Home { get; set; }
+        public Club Away { get; set; }
         public DateTime Time { get; set; }
         public string Judge { get; set; }
         public string Weather { get; set; }
